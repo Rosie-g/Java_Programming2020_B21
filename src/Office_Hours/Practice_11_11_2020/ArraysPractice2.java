c0410d38dd289b4d44bbb52839f110c49ac5e902
Rosie-g <ruz0408@gmail.com>
2020-11-11 12:38:13 -0800

    ArraysPractice2

---
+++ src/Office_Hours/Practice_11_11_2020/ArraysPractice2.java
@@ -0,0 +1,23 @@
+package Office_Hours.Practice_11_11_2020;
+
+public class ArraysPractice2 {
+
+    public static void main(String[] args) {
+
+        String[] word = {"Anna", "Level", "Lol", "Donald", "Biden", "Amir", "Agalar", "Engin","Maryam"};
+
+        int count = 0;
+
+        for (String each : word){ // "Anna"
+            char firstChar = each.toLowerCase().charAt(0); // a
+            char lastChar = each.toLowerCase().charAt(each.length()-1); // a
+
+            if(firstChar==lastChar){
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+    }
+}
