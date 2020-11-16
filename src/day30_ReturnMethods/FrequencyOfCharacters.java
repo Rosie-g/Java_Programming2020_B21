fdf9618c56941998ce082235d49f4357d1a02ba7
Rosie-g <ruz0408@gmail.com>
2020-11-16 12:56:34 -0800

    Day30

---
+++ src/day30_ReturnMethods/FrequencyOfCharacters.java
@@ -0,0 +1,43 @@
+package day30_ReturnMethods;
+
+import library.StringUtility;
+
+public class FrequencyOfCharacters {
+
+    public static void main(String[] args) {
+
+        String str = "aaabbbbcc";
+
+        String result = ""; // a3b4c2
+
+        String nonDup = StringUtility.removeDuplicates(str); // a b c
+
+        for (int i = 0; i <=nonDup.length()-1 ; i++) {
+            char ch = nonDup.charAt(i); // a
+            int count = StringUtility.frequency(str, ch); // 3
+            result += "" + ch + count;
+        }
+        System.out.println(result);
+
+        System.out.println("=====================================");
+
+        String str2 = "aaaaaaaaaaaaaaaabbbbbbbbbbbbbcccccccccddddddddddd";
+        String result2 = frequencyOfCharacters(str2);
+        System.out.println(result2);
+
+    }
+
+    public static String frequencyOfCharacters(String str){
+        String result = ""; // a3b4c2
+
+        String nonDup = StringUtility.removeDuplicates(str); // a b c
+
+        for (int i = 0; i <=nonDup.length()-1 ; i++) {
+            char ch = nonDup.charAt(i); // a
+            int count = StringUtility.frequency(str, ch); // 3
+            result += "" + ch + count;
+        }
+        return result;
+
+    }
+}
