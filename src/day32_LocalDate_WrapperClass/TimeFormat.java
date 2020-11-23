40e532e06ab46d4b83453035f38932536dba7e81
Rosie-g <ruz0408@gmail.com>
2020-11-23 11:16:58 -0800

    Day31

--- src/day32_LocalDate_WrapperClass/TimeFormat.java
+++ src/day32_LocalDate_WrapperClass/TimeFormat.java
@@ -45,5 +45,52 @@ public class TimeFormat {
 
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("EEEE, hh:mm a MMM/dd/yyyy");
         System.out.println(dt1.format(dtf));
+
+        // task:
+
+        LocalDate tomorrow = LocalDate.of(2020, 11, 24);
+        System.out.println(tomorrow.format(df));
+
+        boolean r1 = today.isBefore(tomorrow);// true
+        boolean r2 = today.isAfter(tomorrow);// false
+
+        System.out.println(r1);
+        System.out.println(r2);
+
+        // calculate the age:
+
+        int currentYear = LocalDate.now().getYear();
+        LocalDate DofB = LocalDate.of(2000, 5, 19);
+
+        int age = currentYear - DofB.getYear();
+        System.out.println(age);
+
+        System.out.println(DofB.plusDays(10));
+
+        LocalDate rightNow = LocalDate.now();
+
+        LocalDate holidayBreak = rightNow.plusDays(2);
+        System.out.println(holidayBreak);
+
+        LocalTime time = LocalTime.now();
+
+        LocalTime lunchBreak = time.plusMinutes(15);
+        System.out.println(lunchBreak.format(tf));
+
+        System.out.println("==================================");
+
+        LocalTime classStarts = LocalTime.of(10, 0);
+        LocalTime firstBreak = classStarts.plusMinutes(45);
+        System.out.println(firstBreak);
+
+
+        LocalTime secondBreak = firstBreak.plusHours(1);
+        System.out.println(secondBreak);
+
+        LocalTime thirdBreak=secondBreak.plusHours(1).plusMinutes(15);
+        System.out.println(thirdBreak);
+
+        LocalTime afternoonClass=thirdBreak.plusHours(1);
+        System.out.println(afternoonClass);
     }
 }
