6d9706d3b495c6213b48793d479433808ccb637e
Rosie-g <ruz0408@gmail.com>
2020-11-24 08:05:58 -0800

    Day33

--- src/day33_ArrayList/DateTimePractice.java
+++ src/day33_ArrayList/DateTimePractice.java
@@ -73,14 +73,5 @@ public class DateTimePractice {
                 ex:
                     November/25, Wednesday
 
-3. write a program that can return the sum of all the digits from a string
-            ex:
-                input: "a1b2c3"
-                output: 6
-                    (1+2+3= 6)
-                input: "Today's date is 04/27/2020"
-                output: 17
-                    (0+4+2+7+2+0+2+0=17)
-
  */
 
