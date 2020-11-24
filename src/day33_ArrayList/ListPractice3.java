051ec087d22633466f608313fb5a54ac18928f45
Rosie-g <ruz0408@gmail.com>
2020-11-24 13:05:22 -0800

    Day33

---
+++ src/day33_ArrayList/ListPractice3.java
@@ -0,0 +1,39 @@
+package day33_ArrayList;
+
+import java.util.ArrayList;
+
+public class ListPractice3 {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Katy"); // 0
+        names.add("Amir"); // 1
+        names.add("Nadira"); // 2
+        names.add("Erjon"); // 3
+        names.add("Amir"); // 4
+        names.add("Abek"); // 5
+        names.add("Ilgar"); // 6
+
+        System.out.println(names);
+
+        names.remove(1);
+
+        System.out.println(names);
+
+        names.remove(4);
+        System.out.println(names);
+
+        names.remove(names.size()-1);
+
+        System.out.println(names);
+
+        names.clear();
+
+        System.out.println(names);
+
+
+
+
+    }
+}
