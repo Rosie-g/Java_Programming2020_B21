649611fa7a05d8a23edca662c78611265a0a1756
Rosie-g <ruz0408@gmail.com>
2020-12-01 09:47:11 -0800

    Day35

---
+++ src/day35_CustomClass/Car.java
@@ -0,0 +1,12 @@
+package day35_CustomClass;
+
+public class Car {
+
+   public String brand; // instance variables
+   public String model;
+
+   public void setInfo(String carBrand, String carModel){
+      brand = carBrand;
+      model = carModel;
+   }
+}
---
+++ src/day35_CustomClass/CarObjects.java
@@ -0,0 +1,30 @@
+package day35_CustomClass;
+
+public class CarObjects {
+
+    public static void main(String[] args) {
+        Car car1 = new Car(); // has own brand
+        Car car2 = new Car(); // has own brand
+        Car car3 = new Car(); // has own brand
+
+        car1.brand="Toyota";
+        car2.brand = "BMW";
+        car3.brand = "Lexus";
+        System.out.println(car1.brand);
+        System.out.println(car2.brand);
+        System.out.println(car3.brand);
+
+        car1.model = "Corolla";
+        car2.model = "i8";
+        car3.model = "RX";
+
+        System.out.println(car1.model);
+        System.out.println(car2.model);
+        System.out.println(car3.model);
+
+        Car car4 = new Car();
+        car4.setInfo("Mercedes","C300");
+        System.out.println(car4.brand);
+        System.out.println(car4.model);
+    }
+}
