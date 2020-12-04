153c18d2841121d6deea580baf53db54bfa3dfa3
Rosie-g <ruz0408@gmail.com>
2020-12-04 13:27:01 -0800

    Day37

---
+++ src/day37_CustomClass/Employee.java
@@ -0,0 +1,36 @@
+package day37_CustomClass;
+
+public class Employee {
+
+    public String name, companyName, jobTitle, ID, gender;
+    public int weeklyHours;
+    public double hourlyRate, salary;
+
+
+    public void setInfo(String name, String companyName, String jobTitle, String ID, String gender, int weeklyHours, double hourlyRate) {
+        this.name = name;
+        this.companyName = companyName;
+        this.jobTitle = jobTitle;
+        this.ID = ID;
+        this.gender = gender;
+        this.weeklyHours = weeklyHours;
+        this.hourlyRate = hourlyRate;
+        this.salary = calculateSalary();
+    }
+
+    public double calculateSalary() {
+        return hourlyRate * weeklyHours * 52;
+    }
+
+    public double calculateTax(double stateTaxRate, double federalTaxRate) {
+
+        return calculateSalary() * stateTaxRate + calculateSalary() * federalTaxRate;
+    }
+
+
+
+}
+/*
+        setInfo
+        toString
+ */
