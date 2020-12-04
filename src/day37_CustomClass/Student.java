27fca729fa66f5474ac45fccbbae07d836a256f9
Rosie-g <ruz0408@gmail.com>
2020-12-04 08:30:29 -0800

    Day37

---
+++ src/day37_CustomClass/Student.java
@@ -0,0 +1,34 @@
+package day37_CustomClass;
+
+import java.time.LocalDate;
+
+public class Student {
+
+    public String name, ID;
+    public LocalDate DofB;
+    public int age;
+
+    public char gender;
+    public double gpa;
+
+    public void setInfo(String name, char gender, LocalDate DofB, String ID, double gpa) {
+        this.name = name;
+        this.gender = gender;
+        this.DofB = DofB;
+        age = LocalDate.now().getYear() - DofB.getYear();
+        this.ID = ID;
+        this.gpa = gpa;
+    }
+
+    public String toString() {
+        return
+                "name: " + name  +
+                ", ID: " + ID +
+                ", Date of Birth: " + DofB +
+                ", age: " + age +
+                ", gender: " + gender +
+                ", gpa: " + gpa;
+    }
+
+
+}
