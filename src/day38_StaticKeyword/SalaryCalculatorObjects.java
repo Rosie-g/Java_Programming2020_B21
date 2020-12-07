08f39c76dd9dd38f8338d767c54020cec6b4dbd5
Rosie-g <ruz0408@gmail.com>
2020-12-07 09:40:29 -0800

    Day38

--- src/day38_StaticKeyword/SalaryCalculatorObjects.java
+++ src/day38_StaticKeyword/SalaryCalculatorObjects.java
@@ -9,6 +9,11 @@ public class SalaryCalculatorObjects {
 
         System.out.println(salary1);
 
+        SalaryCalculator salary2 = new SalaryCalculator();
+        salary2.setInfo(55,40,8,22);
+
+        System.out.println(salary1.equals(salary2));
+
 
 
     }
