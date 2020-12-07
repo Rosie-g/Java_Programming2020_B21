64f11abeebec5bac8bf5b60a66f1e15ec1f4bae7
Rosie-g <ruz0408@gmail.com>
2020-12-07 09:55:37 -0800

    Day38

---
+++ src/day38_StaticKeyword/Iphone.java
@@ -0,0 +1,13 @@
+package day38_StaticKeyword;
+
+public class Iphone {
+
+
+}
+/*
+iphone:
+    attributes:
+            instance: model, color, price, storage
+            static: brand, operating system, madeIn, deignated
+
+ */
