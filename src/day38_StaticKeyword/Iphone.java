f6e4ed5f83d6bff54ca40d360a41008af4c4e1fa
Rosie-g <ruz0408@gmail.com>
2020-12-07 11:13:30 -0800

    Day38

--- src/day38_StaticKeyword/Iphone.java
+++ src/day38_StaticKeyword/Iphone.java
@@ -2,7 +2,30 @@ package day38_StaticKeyword;
 
 public class Iphone {
 
+    public String model, color, storage;
+    public double price;
 
+    public static String brand = "Apple", OS = "IOS", madeIn = "China";
+
+    public void call(long phoneNumber) {
+        System.out.println("Iphone " + model + " ia calling " + phoneNumber);
+    }
+
+    public static void getInfo(){
+        System.out.println("Brand: "+brand);
+        System.out.println("Operating System: "+OS);
+        System.out.println("Made in: "+madeIn);
+    }
+
+
+    public String toString() {
+        return "Iphone{" +
+                "brand=" + brand +
+                "model=" + model +
+                ", color=" + color +
+                ", price=" + price +
+                ", Operating System=" + OS ;
+    }
 }
 /*
 iphone:
