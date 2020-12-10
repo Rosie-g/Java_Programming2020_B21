5243d23563727789749ab5d71a37fbe2c0d8bb55
Rosie-g <ruz0408@gmail.com>
2020-12-10 09:27:06 -0800

    Day39

---
+++ src/day39_ConstructorIntro/StudentObject.java
@@ -0,0 +1,12 @@
+package day39_ConstructorIntro;
+
+public class StudentObject {
+
+    public static void main(String[] args) {
+
+
+        CybertekStudent student1 = new CybertekStudent();
+
+        System.out.println(student1);
+    }
+}
