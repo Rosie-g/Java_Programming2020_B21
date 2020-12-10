12e4eeceb0512dc3fe1674bb7cbe94d07e5d9135
Rosie-g <ruz0408@gmail.com>
2020-12-10 08:49:37 -0800

    Day39

---
+++ src/day39_ConstructorIntro/StaticBlocks.java
@@ -0,0 +1,45 @@
+package day39_ConstructorIntro;
+
+import day37_CustomClass.Employee;
+
+public class StaticBlocks {
+
+    public static String company;
+    public static Employee employee1;
+    public static int b;
+    public static boolean isEmployed;
+
+    public int a;
+
+    // xcelSheet
+
+    static {
+        company = "Capital one";
+        employee1 = new Employee();
+        employee1.setInfo("John", company, "SDET", "A123", "Male", 40,55 );
+
+        //  a = 100;
+        b = 200;
+
+        System.out.println("Static Block");
+
+    }
+     /*
+    public static void main(String[] args) {
+        company = "Capital One";
+        isEmployeed = true;
+        employee1 = new Employee();
+        employee1.setInfo("john", company, "SDET", "A123", "Male", 40, 55);
+        b = 200;
+        System.out.println(company);
+        System.out.println(employee1);
+        System.out.println(b);
+        System.out.println(isEmployeed);
+    }
+*/
+
+
+
+
+
+}
