f12dafed320ddbf00e3a9f51bce6f18a16de2f15
Rosie-g <ruz0408@gmail.com>
2020-12-14 08:43:30 -0800

    Day41

---
+++ src/day41_Encapsulation/Data.java
@@ -0,0 +1,11 @@
+package day41_Encapsulation;
+
+public class Data {
+
+    public static String publicVariable = "A";
+    private static String privateVariable = "B";
+    static String defaultVariable = "C"; // default access
+
+    // protected  static String protectedVariable  = "D";
+
+}
---
+++ src/day41_Encapsulation/Test.java
@@ -0,0 +1,12 @@
+package day41_Encapsulation;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println(Data.publicVariable);
+
+        System.out.println(Data.defaultVariable);
+
+    }
+}
