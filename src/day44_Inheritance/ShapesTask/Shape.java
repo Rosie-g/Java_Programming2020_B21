df6bca1f8f050978c641f517556b8c31a905df32
Rosie-g <ruz0408@gmail.com>
2020-12-21 07:25:54 -0800

    Day44

---
+++ src/day44_Inheritance/ShapesTask/Shape.java
@@ -0,0 +1,49 @@
+package day44_Inheritance.ShapesTask;
+
+
+public class Shape {
+
+    public String name;
+    public static boolean isShape, hasArea, hasPerimeter;
+
+    static {
+        isShape = true;
+        hasArea = true;
+        hasPerimeter = true;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public double calcArea(){
+        return 0;
+    }
+
+    public double calcPerimeter(){
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "name='" + name + '\'' +
+                "area='" + calcArea() + '\'' +
+                "perimeter='" + calcPerimeter() + '\'' +
+                '}';
+    }
+}
+/*
+1. create a class called Shape
+                        variables: name, isShape(boolean), hasArea(boolean), hasPerimeter(boolean)
+                        methods: setInfo, calcArea(), calcPerimeter(),
+          toString(): returns the name, area and perimeter that are returned by calcArea(), calcPerimeter()
+
+2. create sub classes of Shape:
+                        Circle
+                        Rectangle
+                        Square
+             give the instance variables that are needed to calculate the Area, perimeter, of those shapes
+
+ */
