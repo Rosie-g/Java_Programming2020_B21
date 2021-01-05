d151a92bc9c28f50e136274f85cc492c7020a8a4
Rosie-g <ruz0408@gmail.com>
2021-01-05 21:06:24 +0400

    Day45

---
+++ src/day45_Exceptions/PhoneTask/ExceptionsIntro.java
@@ -0,0 +1,13 @@
+package day45_Exceptions.PhoneTask;
+
+public class ExceptionsIntro {
+
+    public static void main(String[] args) {
+
+        int[] array = {1, 2, 3, 4, 5};
+
+        System.out.println(array[100]);
+
+        System.out.println(10 / 2);
+    }
+}
