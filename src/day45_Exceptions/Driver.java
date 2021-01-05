d7c51df451be542aa8a57b020aea301f1d4ee4b9
Rosie-g <ruz0408@gmail.com>
2021-01-06 00:49:03 +0300

    Day45

---
+++ src/day45_Exceptions/Driver.java
@@ -0,0 +1,35 @@
+package day45_Exceptions;
+
+public class Driver {
+
+    public static void main(String[] args) {
+
+
+        String browserName = "Chrome";
+
+        switch (browserName.toLowerCase()){
+            case "chrome":
+                System.out.println("Chrome is selected");
+                break;
+
+            case "firefox":
+                System.out.println("Firefox is selected");
+                break;
+
+            case "safari":
+                System.out.println("Safari is selected");
+                break;
+
+            case "opera":
+                System.out.println("Opera is selected");
+                break;
+
+            default:
+                throw new RuntimeException("No such a browser named "+browserName);
+        }
+
+        System.out.println("----------------------------------------");
+
+        throw new ClassCastException("Invalid Classes");
+    }
+}
