e68d1d48a2a563904a46ee5e7db0cd895a837c71
Rosie-g <ruz0408@gmail.com>
2021-01-09 11:27:55 +0300

    Day46

--- src/day46_JavaRecap/CarTask/BMW.java
+++ src/day46_JavaRecap/CarTask/BMW.java
@@ -1,15 +1,15 @@
 package day46_JavaRecap.CarTask;
 
-public class BMW extends Car{
+public class BMW extends Car {
     public BMW(String model, String color, int year, double price) {
         super("BMW", model, color, year, price);
     }
 
     public void start() {
-        System.out.println("Starting "+brand+" "+model+" by calling mechanic");
+        System.out.println("Starting " + brand + " " + model + " by calling mechanic");
     }
 
-    public void autoPark(){
-        System.out.println("Auto parking "+brand+" "+model);
+    public void autoPark() {
+        System.out.println("Auto parking " + brand + " " + model);
     }
 }
