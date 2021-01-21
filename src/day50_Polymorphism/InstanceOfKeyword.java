ee7b42f48fed15b9a0df45f102b08372348d21b4
Rosie-g <ruz0408@gmail.com>
2021-01-21 09:59:04 -0800

    Day50

---
+++ src/day50_Polymorphism/InstanceOfKeyword.java
@@ -0,0 +1,24 @@
+package day50_Polymorphism;
+
+import day48_Abstrsaction.AnimalTask.Animal;
+import day48_Abstrsaction.AnimalTask.Dog;
+import day48_Abstrsaction.AnimalTask.Dolphin;
+import day48_Abstrsaction.AnimalTask.Eagle;
+
+public class InstanceOfKeyword {
+
+    public static void main(String[] args) {
+
+        Animal animal = new Eagle("xx", "yy", "t", 'f', 12);
+
+        boolean isDog = animal instanceof Dog;
+        boolean isDolphin = animal instanceof Dolphin;
+        boolean isEagle = animal instanceof Eagle;
+        boolean isAnimal = animal instanceof Animal;
+
+        System.out.println("isDog = " + isDog);
+        System.out.println("isDolphin = " + isDolphin);
+        System.out.println("isEagle = " + isEagle);
+        System.out.println("isAnimal = " + isAnimal);
+    }
+}
