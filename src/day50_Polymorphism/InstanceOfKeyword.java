74986a61482be845c7a1c9d0207ff81bedee06c2
Rosie-g <ruz0408@gmail.com>
2021-01-27 15:13:48 -0800

    Day50

--- src/day50_Polymorphism/InstanceOfKeyword.java
+++ src/day50_Polymorphism/InstanceOfKeyword.java
@@ -1,9 +1,9 @@
 package day50_Polymorphism;
 
-import day48_Abstrsaction.AnimalTask.Animal;
-import day48_Abstrsaction.AnimalTask.Dog;
-import day48_Abstrsaction.AnimalTask.Dolphin;
-import day48_Abstrsaction.AnimalTask.Eagle;
+import day48_Abstraction.AnimalTask.Animal;
+import day48_Abstraction.AnimalTask.Dog;
+import day48_Abstraction.AnimalTask.Dolphin;
+import day48_Abstraction.AnimalTask.Eagle;
 
 public class InstanceOfKeyword {
 
